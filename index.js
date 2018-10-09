520218f904e7007f5cd426a5a8801773b127d94c
stupid cat <cat@blargbot.xyz>
2018-10-09 13:12:37 -0600

    fix autoplay

--- index.js
+++ index.js
@@ -435,7 +435,7 @@ You can execute up to two commands in a single message by separating them with \
             let [card] = await game.deal(game.player, 1);
             if (game.rules.DRAW_AUTOPLAY === true && !card.wild
                 && (!game.flipped.color || card.id === game.flipped.id || card.color === game.flipped.color)) {
-                return await commands.play(msg, [card.toString()], true);
+                return await commands.play(msg, [card.toString().split(' ')], true);
             }
             let player = game.player;
             await game.next();
